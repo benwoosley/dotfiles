14787106595500c5196ddbd611bc43ce0190a3c7
Benjamin Woosley <benjaminwoosley@gmail.com>
2022-08-23 11:18:49 -0400

    add null-ls config

---
+++ nvim/after/plugin/null.lua
@@ -0,0 +1,33 @@
+local null_ls = require("null-ls")
+local formatting = null_ls.builtins.formatting
+
+local sources = {
+	formatting.stylua,
+	formatting.black,
+	formatting.clang_format.with({
+		extra_args = {
+			"--style",
+			"{IndentWidth: 4, BreakBeforeBraces: Allman}",
+		},
+	}),
+}
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+null_ls.setup({
+	sources = sources,
+
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
+})
