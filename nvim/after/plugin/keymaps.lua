e1ff08673f772e62de666df798f362b6e4bc04ae
Benjamin Woosley <benjaminwoosley@gmail.com>
2022-09-14 22:10:20 -0400

    update nvim config

---
+++ nvim/after/plugin/keymaps.lua
@@ -0,0 +1,37 @@
+vim.cmd([[
+let mapleader=" "
+cnoreabbrev w' w
+" Find files using Telescope command-line sugar.
+nnoremap <leader>ff <cmd>Telescope find_files<cr>
+nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+nnoremap <leader>fb <cmd>Telescope buffers<cr>
+nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+
+map <F5> :call CompileRunGcc()<CR>
+func! CompileRunGcc()
+exec "w"
+if &filetype == 'c'
+exec "!gcc % -o %<"
+exec "!time ./%<"
+elseif &filetype == 'cpp'
+exec "!g++ % -o %<"
+exec "!time ./%<"
+elseif &filetype == 'java'
+exec "!javac %"
+exec "!time java -cp %:p:h %:t:r"
+elseif &filetype == 'sh'
+exec "!time bash %"
+elseif &filetype == 'python'
+exec "!time python3 %"
+elseif &filetype == 'html'
+exec "!open -a \"Safari.app\" % &"
+elseif &filetype == 'go'
+exec "!go build %<"
+exec "!time go run %"
+elseif &filetype == 'mkd'
+exec "!~/.vim/markdown.pl % > %.html &"
+exec "!open -a \"Safari.app\" %.html &"
+endif
+endfunc
+
+]])
---
+++ nvim/ftdetect/ampl.vim
@@ -0,0 +1,1 @@
+autocmd BufNewFile,BufRead,BufReadPost *mod,*.dat set syntax=ampl
